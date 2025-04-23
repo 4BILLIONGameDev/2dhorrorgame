26fccfee5951ee5305c88ef0590fe0525b38bb5e
dippydro22 <yuza0522@gmail.com>
2025-04-23 21:03:55 +0900

    Player 움직임

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public Vector2 GetMovementInput()
+    {
+        //상하 좌우 이동
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    public bool GetInteractPressed()
+    {
+        //E키 상호작용
+        return Input.GetKeyDown(KeyCode.E);
+    }
+
+    public bool GetObserverSwitch()
+    {
+        //탭 키 상호작용
+        return Input.GetKeyDown(KeyCode.Tab); 
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 3f;
+
+
+
+    private Rigidbody2D rigidbody;
+    private Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = InputManager.Instance.GetMovementInput().normalized;
+        // 움직임 처리
+        dirDetermine(ref direction);
+        if (InputManager.Instance.GetInteractPressed())
+        {
+            // 문 열기, 아이템 줍기 등
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.velocity = direction * moveSpeed;
+    }
+
+    void dirDetermine(ref Vector2 dir)
+    {
+        if (Mathf.Abs(dir.x) > 0.1f && Mathf.Abs(dir.y) > 0.1f)
+        {
+            // 여기선 가로 우선 (x축)
+            dir.y = 0f;
+        }
     }
 }
