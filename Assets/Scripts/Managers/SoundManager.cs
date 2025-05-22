4453fe7ebdfd743de3b7641ac249b3675d9b2be7
tjrdn123 <tjrdn125125@gmail.com>
2025-05-22 21:16:14 +0900

    ui 설정

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class SoundManager : MonoBehaviour
@@ -8,6 +9,10 @@ public class SoundManager : MonoBehaviour
     //오디오 소스
     public AudioSource bgmSource;
     public AudioSource sfxSource;
+
+    private Slider masterSlider, bgmSlider, sfxSlider;
+    private TextMeshProUGUI masterText, bgmText, sfxText;
+
 
     private float masterVolume = 1f;
     private float bgmVolume = 1f;
@@ -24,9 +29,17 @@ public class SoundManager : MonoBehaviour
     }
 
     private void Start()
-    {
+    {   // 슬라이더, 텍스트 자동 참조 (오브젝트에 이름 주의)
+        masterSlider = GameObject.Find("masterVolume").GetComponent<Slider>();
+        bgmSlider = GameObject.Find("bgmVolume").GetComponent<Slider>();
+        sfxSlider = GameObject.Find("sfxVolume").GetComponent<Slider>();
+
+        masterText = GameObject.Find("MasterText").GetComponent<TextMeshProUGUI>();
+        bgmText = GameObject.Find("BGMText").GetComponent<TextMeshProUGUI>();
+        sfxText = GameObject.Find("SFXText").GetComponent<TextMeshProUGUI>();
         // 초기값 반영
         ApplyVolumes();
+        UpdateVolumeTexts();
 
 
     }
@@ -41,18 +54,21 @@ public class SoundManager : MonoBehaviour
     {
         bgmVolume = Mathf.Clamp01(volume);
         ApplyVolumes();
+        UpdateVolumeTexts();
     }
 
     public void SetSFXVolume(float volume)
     {
         sfxVolume = Mathf.Clamp01(volume);
         ApplyVolumes();
+        UpdateVolumeTexts();
     }
 
     public void ToggleMasterMute(bool isMuted)//사운드 토글
     {
         isMasterMuted = isMuted;
         ApplyVolumes();
+        UpdateVolumeTexts();
     }
 
     public void ToggleBGMMute(bool isMuted)
@@ -91,5 +107,16 @@ public class SoundManager : MonoBehaviour
             else
                 sfxSource.volume = sfxVolume * master;
         }
+    }
+    private void UpdateVolumeTexts()
+    {
+        if (masterText != null)
+            masterText.text = Mathf.RoundToInt(masterVolume * 100).ToString();
+
+        if (bgmText != null)
+            bgmText.text = Mathf.RoundToInt(bgmVolume * 100).ToString();
+
+        if (sfxText != null)
+            sfxText.text = Mathf.RoundToInt(sfxVolume * 100).ToString();
     }
 }
